9bf1f9a464f451767d6f882e10d5aafef0aa878d
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-05-05 17:57:15 +0100

    added login logic

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,15 +1,54 @@
 import React, { useState } from 'react';
 import CompleteLogo from "../../assets/logo.svg"
 import LoginGif from "../../assets/login.gif"
+import Eye from "../../assets/eye.svg"
+import EyeSlash from  "../../assets/eye-slash.svg"
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        // Handle your login logic here
-        console.log('Login Info', { email, password });
+        setErrorMessage('');
+
+        try {
+            const body = JSON.stringify({
+                email: email,
+                password: password,
+            });
+            const response = await fetch('http://localhost:3000/user/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body
+            });
+
+            if (!response.ok) {
+                if (response.status === 401) {
+                    setErrorMessage("Wrong credentials. Please try again.");
+                } else {
+                    setErrorMessage(`Please Verify Your Internet Connections`);
+                }
+                return;
+            }
+
+            const data = await response.json();
+            console.log('Login successful:', data);
+
+
+        } catch (error) {
+            setErrorMessage('An error occurred. Please try again.');
+            console.error('Login failed:', error);
+
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
     };
 
     return (
@@ -31,7 +70,7 @@ const LoginPage = () => {
                     </div>
                     <div className="flex flex-col justify-center items-center w-full h-full">
                         <h2 className="text-3xl font-bold text-[#263238] mb-8 self-start">Login Into Your Account</h2>
-                        <form onSubmit={handleSubmit} className="w-full flex-col">
+                            <form onSubmit={handleSubmit} className="w-full flex-col">
                             <div className="mb-4">
                                 <label htmlFor="email" className="block mb-2 text-[#263238]">
                                     Email Address
@@ -46,35 +85,45 @@ const LoginPage = () => {
                                     required
                                 />
                             </div>
-                            <div className="mb-6">
-                                <label htmlFor="password" className="block mb-2 text-[#263238]">
-                                    Password
-                                </label>
-                                <input
-                                    id="password"
-                                    type="password"
-                                    className="w-full p-3 border border-gray-300 rounded-md"
-                                    placeholder="Enter your password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                                <div className="text-right mt-2">
-                                    <a href="#" className="text-sm text-[#80b38f] hover:underline"
-                                       style={{color: "#63C474"}}>
-                                        Forget the password?
-                                    </a>
+                                <div className="mb-6">
+                                    <div className="flex justify-between items-center mb-2">
+                                        <label htmlFor="password" className="block text-[#263238]">
+                                            Password
+                                        </label>
+                                        <div>
+                                            <img src={passwordVisible ? EyeSlash : Eye}
+                                                 className={"w-[2vw] h-[1.2vw] mr-4 hover:cursor-pointer"}
+                                                 onClick={togglePasswordVisibility} alt="password"/>
+                                        </div>
+                                    </div>
+                                    <input
+                                        id="password"
+                                        type={passwordVisible?"text":"password"}
+                                        className="w-full p-3 border border-gray-300 rounded-md"
+                                        placeholder="Enter your password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <div className="text-right mt-2">
+                                        <a href="/" className="text-sm text-[#80b38f] hover:underline"
+                                           style={{color: "#63C474"}}>
+                                            Forget the password?
+                                        </a>
+                                    </div>
+                                    <div className="mb-4 text-red-500 w-full h-[4vh] mt-4">{errorMessage ?? null}</div>
+
                                 </div>
-                            </div>
-                            <div className="mb-4 mt-10">
-                                <button
-                                    type="submit"
-                                    className="w-full p-3 text-white bg-green-500 rounded-md hover:bg-green-700"
-                                    style={{backgroundColor: "#84ca90"}}
-                                >
-                                    Submit
-                                </button>
-                            </div>
+                                <div className="mb-4 mt-5">
+                                    <button
+                                        type="submit"
+                                        className="w-full p-3 text-white bg-[#84ca90]
+                                        rounded-md hover:bg-green-600"
+
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
                         </form>
                         <div className="mt-4 text-left">
                             <a href="#" className="text-sm text-[#80b38f]" style={{color: "#63C474"}}>
