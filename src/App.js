f9fa057290b2b9a559124c2a0a8f66fd1567b7a6
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-05-05 23:03:28 +0100

    adding navbar layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import Navbar from "./components/navbar/navbar";
 import './App.css';
+
 
 function App() {
   return (
-   <>
-   </>
+      <>
+              <Navbar></Navbar>
+              <div className="ml-[20vw] bg-[#4B4B4B33]">
+              <div className="w-full h-[200vh] "></div>
+              </div>
+
+
+
+      </>
   );
 }
 
