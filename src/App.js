43e192c72b2a2d0f0cfbee403263e8fb857bfb79
khalil207 <khalil.kdous@insat.ucat.tn>
2024-05-06 17:16:18 +0100

    added service list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import Navbar from "./components/navbar/navbar";
 import './App.css';
+import ServiceList from "./components/navbar/ServiceList";
 
 
 function App() {
   return (
       <>
-              <Navbar></Navbar>
-              <div className="ml-[20vw] bg-[#4B4B4B33]">
-                  {/*Hotou lcomponents teekom houni!!!!!!!!!!!!!!!!!!*/}
-              </div>
-
-
-
+          <Navbar></Navbar>
+          <div className="ml-[20vw] bg-gray-200 p-4">
+            <ServiceList /> {ServiceList}
+          </div>
       </>
   );
 }
