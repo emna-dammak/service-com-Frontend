40aa5098f9e541ed5adac6f34fcc68a5c80d25c3
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-06-03 00:54:05 +0100

    fixing merge login-chat bug

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthPage from "./components/Auth/Auth";
 import ChatInterface from "./components/chatInterface";
-import Service from './service';
 import Navbar from "./components/navbar/navbar";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AuthPage />}></Route>
-        <Route path="/chat" element={<ChatInterface />}></Route>
+
+        <Route path="/chat" element={<> <Navbar></Navbar>
+            <div className="ml-[20vw] bg-[#4B4B4B33]">
+                <ChatInterface />
+            </div></>}></Route>
       </Routes>
     </Router>
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Service from './service'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Service/>
+    <App />
   </React.StrictMode>
 );
 
